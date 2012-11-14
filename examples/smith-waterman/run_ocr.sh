df2f7d3a726f5a1e04d72de3a60cdcc4d6d2be4a
Sagnak Tasirlar <sagnaks@gmail.com>
2012-11-14 04:22:29 -0600

    fix Makefile for the omp rules to include the omp files make run_ocr script clean the environment and build the ocr executable add the run_omp script that cleans the environment and builds the omp benchmark, then runs it with various number of omp threads

--- examples/smith-waterman/run_ocr.sh
+++ examples/smith-waterman/run_ocr.sh
@@ -4,6 +4,7 @@ INPUT_2=./string2-larger.txt
 TILE_WIDTH=290
 TILE_HEIGHT=300
 
+make clean; make compile
 for MACH in 16 8 4 2 1; do
         echo "using $MACH"
         $EXEC -md mach_$MACH.xml $INPUT_1 $INPUT_2 $TILE_WIDTH $TILE_HEIGHT | sed -e '/^Size.*/ d' -e '/^Imported.*/ d' -e '/^Allocat.*/ d' -e '/^Tile.*/ d'
---
+++ examples/smith-waterman/run_omp.sh
@@ -0,0 +1,11 @@
+EXEC=./smith_waterman_tiled_omp
+INPUT_1=./string1-larger.txt
+INPUT_2=./string2-larger.txt
+TILE_WIDTH=290
+TILE_HEIGHT=300
+
+make clean; make compile_omp 
+for MACH in 16 8 4 2 1; do
+        echo "using $MACH"
+        /bin/env OMP_NUM_THREADS=$MACH $EXEC $INPUT_1 $INPUT_2 $TILE_WIDTH $TILE_HEIGHT | sed -e '/^Size.*/ d' -e '/^Imported.*/ d' -e '/^Allocat.*/ d' -e '/^Tile.*/ d'
+done
