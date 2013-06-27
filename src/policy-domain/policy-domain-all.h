831d2433ee6552e239063d1e419113d246c94c40
vc8 <vc8@rice.edu>
2013-06-26 23:51:22 -0500

    (xstack-intel) cleanup commented dead code

--- src/policy-domain/policy-domain-all.h
+++ src/policy-domain/policy-domain-all.h
@@ -80,67 +80,6 @@ inline ocrPolicyDomainFactory_t * newPolicyDomainFactory(policyDomainType_t type
     return NULL;
 }
 
-// ocrPolicyDomain_t * newPolicyDomain(ocr_policy_domain_kind policyType,
-//                                 u64 workpileCount,
-//                                 u64 workerCount,
-//                                 u64 computeCount,
-//                                 u64 schedulerCount) {
-//     switch(policyType) {
-//     case OCR_POLICY_HC:
-//         return hc_policy_domain_constructor(workpileCount, workerCount,
-//                                             computeCount, schedulerCount);
-//     case OCR_POLICY_XE:
-//         return xe_policy_domain_constructor(workpileCount, workerCount,
-//                                             computeCount, schedulerCount);
-//     case OCR_POLICY_CE:
-//         return ce_policy_domain_constructor(workpileCount, workerCount,
-//                                             computeCount, schedulerCount);
-//     case OCR_POLICY_MASTERED_CE:
-//         return ce_mastered_policy_domain_constructor(workpileCount, workerCount,
-//                                                      computeCount, schedulerCount);
-//     case OCR_PLACE_POLICY:
-//         return place_policy_domain_constructor();
-//     case OCR_LEAF_PLACE_POLICY:
-//         return leaf_place_policy_domain_constructor(workpileCount, workerCount,
-//                                                     computeCount, schedulerCount);
-//     case OCR_MASTERED_LEAF_PLACE_POLICY:
-//         return mastered_leaf_place_policy_domain_constructor(workpileCount, workerCount,
-//                                                              computeCount, schedulerCount);
-//     default:
-//         assert(false && "Unrecognized policy domain kind");
-//         break;
-//     }
-//     return NULL;
-// }
-
-// void ocr_policy_domain_destruct(ocrPolicyDomain_t * policy) {
-//     u64 i;
-//     for(i = 0; i < policy->workerCount; i++) {
-//         policy->workers[i]->fctPtrs->destruct(policy->workers[i]);
-//     }
-//     free(policy->workers);
-
-//     for(i = 0; i < policy->computeCount; i++) {
-//         policy->computes[i]->fctPtrs->destruct(policy->computes[i]);
-//     }
-//     free(policy->computes);
-
-//     for(i = 0; i < policy->workpileCount; i++) {
-//         policy->workpiles[i]->fctPtrs->destruct(policy->workpiles[i]);
-//     }
-//     free(policy->workpiles);
-
-//     for(i = 0; i < policy->schedulerCount; i++) {
-//         policy->schedulers[i]->fctPtrs->destruct(policy->schedulers[i]);
-//     }
-//     free(policy->schedulers);
-
-//     // Destroy the GUID
-//     ocrGuidProvider_t * guidProvider = getCurrentPD()->guidProvider;
-//     guidProvider->fctPtrs->releaseGuid(guidProvider, policy->guid);
-//     free(policy);
-// }
-
 /*
  *
 ocrGuid_t policy_domain_handIn_assert ( ocrPolicyDomain_t * this, ocrPolicyDomain_t * takingPolicy, ocrGuid_t takingWorkerGuid ) {
