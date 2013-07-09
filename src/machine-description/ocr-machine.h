e30bb8eb7c6ac40b29571eb51f04b724170b668c
bala <bala@bseshasa-desk.jf.intel.com>
2013-07-09 11:37:42 -0700

    (xstack-intel) Was unintentionally left out in previous commit

---
+++ src/machine-description/ocr-machine.h
@@ -0,0 +1,40 @@
+#ifndef _CONFIG_H_
+
+#define _CONFIG_H_
+#include <stdio.h>
+#include <string.h>
+#include <iniparser.h>
+#include <signal.h>
+
+#include <ocr-mem-platform.h>
+#include <ocr-mem-target.h>
+#include <ocr-allocator.h>
+#include <ocr-comp-platform.h>
+#include <ocr-comp-target.h>
+#include <ocr-worker.h>
+#include <ocr-workpile.h>
+#include <ocr-scheduler.h>
+#include <ocr-policy-domain.h>
+#include <ocr-datablock.h>
+#include <ocr-event.h>
+
+typedef enum {
+    guid_type,
+    memplatform_type,
+    memtarget_type,
+    allocator_type,
+    compplatform_type,
+    comptarget_type,
+    workpile_type,
+    worker_type,
+    scheduler_type,
+    policydomain_type,
+} type_enum;
+
+typedef struct {
+    type_enum from;
+    type_enum to;
+    char *refstr;
+} dep_t;
+
+#endif
