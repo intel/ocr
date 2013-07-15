aa02ef6c4cf575ac9d32f94f5250b46a897b98ef
Romain <romain.e.cledat@intel.com>
2013-07-15 16:49:44 -0700

    (xstack-intel) Added small helper functions to unparse arguments from the argument data-block

--- inc/ocr.h
+++ inc/ocr.h
@@ -52,4 +52,36 @@
  */
 void ocrShutdown();
 
+/**
+ * @brief Gets the number of arguments packed in the
+ * single data-block passed to the first EDT
+ *
+ * When starting, the first EDT gets passed a single data-block that contains
+ * all the arguments passed to the program. These arguments are packed in the
+ * following format:
+ *     - first 8 bytes: argc
+ *     - argc count of 8 byte values indicating the offsets from
+ *       the start of the data-block to the argument (ie: the first
+ *       8 byte value indicates the offset in bytes to the first argument)
+ *     - the arguments (NULL terminated character arrays)
+ *
+ * This call will extract the number of arguments
+ *
+ * @param dbPtr    Pointer to the start of the argument data-block
+ * @return Number of arguments
+ */
+u64 getArgc(void* dbPtr);
+
+/**
+ * @brief Gets the argument 'count' from the data-block containing the
+ * arguments
+ *
+ * @see getArgc() for an explanation
+ *
+ * @param dbPtr    Pointer to the start of the argument data-block
+ * @param count    Index of the argument to extract
+ * @return A NULL terminated string
+ */
+char* getArgv(void* dbPtr, u64 count);
+
 #endif /* __OCR_H__ */
--- src/api/ocr.c
+++ src/api/ocr.c
@@ -41,3 +41,14 @@ void ocrShutdown() {
     ocrPolicyDomain_t* currPD = getCurrentPD();
     finishAllPD(currPD);
 }
+
+u64 getArgc(void* dbPtr) {
+    return ((u64*)dbPtr)[0];
+}
+
+char* getArgv(void* dbPtr, u64 count) {
+    u64* dbPtrAsU64 = (u64*)dbPtr;
+    ASSERT(count < dbPtrAsU64[0]);
+    u64 offset = dbPtrAsU64[1 + count];
+    return ((char*)dbPtr) + offset;
+}
