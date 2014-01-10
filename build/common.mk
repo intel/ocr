27fa7b8971aee042b82a8f4d334322ef78a9b5d6
Ivan Ganev <ivan.b.ganev@intel.com>
2014-01-10 13:47:29 -0800

    (xstack-intel) Beginnings of the new build system as discussed on 2014-01-08. Current blocking issue is bad relative include paths in the FSIM sources.
    Change-Id: Id571cc2e4aa594fbcb4d45246410407f47f0579f

---
+++ build/common.mk
@@ -0,0 +1,20 @@
+#
+# OCR top level directory
+#
+OCRDIR := ../../runtime/ocr-x86
+
+#
+# Cook a list of all source files and their objects
+#
+SRCS   := $(shell find $(OCRDIR)/src -name '*.[csS]' -print)
+OBJS   := $(addsuffix .o, $(basename $(notdir $(SRCS))))
+
+#
+# Cook a source search path -- all subdirs in the OCR tree
+#
+VPATH  := $(shell find $(OCRDIR)/src -type d -print)
+
+#
+# Global CFLAGS to be passed into all architecture builds
+#
+CFLAGS := -I . -I $(OCRDIR)/inc -I $(OCRDIR)/src/inc -DELS_USER_SIZE=0
