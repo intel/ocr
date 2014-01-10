c12e99cb201f533eabcb65601f8153824aa5353e
Ivan Ganev <ivan.b.ganev@intel.com>
2014-01-10 15:48:28 -0800

    (xstack-intel) Fix license, move objects to subdir, add dependence generation.
    Change-Id: I3d5c28c70e28dcca7626334197ae1cf25a3cc0da

--- build/common.mk
+++ build/common.mk
@@ -4,13 +4,18 @@
 OCRDIR := ../../runtime/ocr-x86
 
 #
-# Cook a list of all source files and their objects
+# Object & dependence file subdirectory
 #
-SRCS   := $(shell find $(OCRDIR)/src -name '*.[csS]' -print)
-OBJS   := $(addsuffix .o, $(basename $(notdir $(SRCS))))
+OBJDIR := objs
 
 #
-# Cook a source search path -- all subdirs in the OCR tree
+# Generate a list of all source files and the respective objects
+#
+SRCS   := $(shell find $(OCRDIR)/src -name '*.[csS]' -print)
+OBJS   := $(addprefix $(OBJDIR)/, $(addsuffix .o, $(basename $(notdir $(SRCS)))))
+
+#
+# Generate a source search path
 #
 VPATH  := $(shell find $(OCRDIR)/src -type d -print)
 
@@ -18,3 +23,30 @@ VPATH  := $(shell find $(OCRDIR)/src -type d -print)
 # Global CFLAGS to be passed into all architecture builds
 #
 CFLAGS := -I . -I $(OCRDIR)/inc -I $(OCRDIR)/src -I $(OCRDIR)/src/inc -DELS_USER_SIZE=0
+
+#
+# Objects build rules
+#
+$(OBJDIR)/%.o: %.c
+	$(CC) $(CFLAGS) -MMD -c $< -o $@
+
+$(OBJDIR)/%.o: %.S
+	$(CC) $(CFLAGS) -MMD -c $< -o $@
+
+#
+# Include auto-generated dependence files
+#
+-include $(wildcard $(OBJDIR)/*.d)
+
+#
+# Build targets
+#
+all: $(OBJS)
+	@echo "$(ARCH): ALL RULE"
+
+debug: $(OBJS)
+	@echo "$(ARCH): DEBUG RULE"
+
+clean:
+	-$(RM) $(RMFLAGS) $(OBJDIR)/*
+
