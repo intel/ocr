03df965d06f546538fe8122243be6fbf0ed479f1
Bala S <bala.seshasayee@intel.com>
2014-02-09 21:54:10 -0800

    (xstack-intel) Minor clean-up of scripts directory
    Change-Id: I81ca8dc292c1d9f05e015219eefda453e678dd16

---
+++ scripts/Blob/build_blob.sh
@@ -0,0 +1,8 @@
+# Generate the CE blob
+../../build/x86-builder-fsim-ce/builder.exe -ocr:cfg fsim_ce.cfg
+
+# Generate the XE blob - TODO
+#../../build/x86-builder-fsim-xe/builder.exe -ocr:cfg fsim_xe.cfg
+
+# Combine the two
+./aggregate_binary_files.sh ce_blob.bin ce_blob.bin dummy ../../../ss/xe-sim/tests/mb-blob/blob
