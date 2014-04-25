d154400daba3d56622d6fa9a96801a99d3bd3c2a
Ivan Ganev <ivan.b.ganev@intel.com>
2014-04-24 23:06:09 -0700

    (xstack-intel) Connect XE sal_assert() to meaningful console message.
    This was borked for too-long because of heap corruption that was
    messing up the unnamed file name strings and wreaking havoc.  The
    previous commit fixed that and this now works.
    
    Change-Id: Iad0cdea9cfa7cd88b49f883c6ca87a8808bd85d7

--- runtime/ocr-x86/src/sal/fsim-xe/ocr-sal-fsim-xe.h
+++ runtime/ocr-x86/src/sal/fsim-xe/ocr-sal-fsim-xe.h
@@ -8,13 +8,20 @@
 #define __OCR_SAL_FSIM_XE_H__
 
 #include "ocr-hal.h"
+#include "xe-abi.h"
 
 #define sal_abort() hal_abort()
 
 #define sal_exit(x) hal_exit(x)
 
-#define sal_assert(x, f, l) if(!(x)) hal_abort()
+#define sal_assert(x, fn, ln) if(!(x)) {                            \
+        __asm__ __volatile__ __attribute__ (( noreturn )) (         \
+            "alarm %2\n\t"                                          \
+            : : "{r2}" (ln), "{r3}" (fn), "L" (XE_ASSERT));         \
+    }
 
-#define sal_print(msg, len) __asm__ __volatile__("alarm 0xFD\n\t" : : "{r2}" (msg), "{r3}" (len))
+#define sal_print(msg, len) __asm__ __volatile__(                   \
+        "alarm %2\n\t"                                              \
+        : : "{r2}" (msg), "{r3}" (len), "L" (XE_CONOUT))
 
 #endif /* __OCR_SAL_FSIM_XE_H__ */
